2bab342bb484f90790af336e3cd48b6c61e67c17
Oleg Ershov <oecorango@gmail.com>
2023-09-13 20:49:29 +0400

    refactor: change tests folders

--- src/tests/Layout.test.tsx
+++ src/tests/Layout.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { Layout } from '../components/Layout';
+import { Layout } from '../Layouts/Layout';
 
 jest.mock('../components/Headers', () => {
   return {
